dc65d99a2831568ce81c3e28a46fdb4350307031
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2020-04-14 17:58:48 +0800

    day6 part 2

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean
+
 clean:
-	@i=1 ; while [[ $$i -le 25 ]] ; do \
+	@for i in $$(seq 1 25); do \
 	  rm -f "day"$$i/"day"$$i ; \
 	  rm -f "day"$$i/*.hi ; \
 	  rm -f "day"$$i/*.o ; \
-	  (( i = i + 1 )) ; \
 	done
--- day6/day6.hs
+++ day6/day6.hs
@@ -21,12 +21,12 @@ data Instruction
   | Toggle { _range :: Range }
   deriving (Show)
 
-toF :: Instruction -> Bool -> Bool
+toF :: Instruction -> Int -> Int
 toF ins =
   case ins of
-    TurnOff _ -> const False
-    TurnOn _ -> const True
-    Toggle _ -> not
+    TurnOff _ -> max 0 . subtract 1
+    TurnOn _ -> (+ 1)
+    Toggle _ -> (+ 2)
 
 toLOCs :: Range -> [LOC]
 toLOCs (Range (x0, y0) (x1, y1)) = [(x, y) | x <- [x0 .. x1], y <- [y0 .. y1]]
@@ -64,14 +64,14 @@ instruction = do
 instructions :: Parsec String u [Instruction]
 instructions = sepEndBy instruction spaces
 
-applyInstructions :: [Instruction] -> AU.UArray LOC Bool
+applyInstructions :: [Instruction] -> AU.UArray LOC Int
 applyInstructions ins =
   AST.runSTUArray $ do
-    arr <- AST.newArray ((0, 0), (999, 999)) False
+    arr <- AST.newArray ((0, 0), (999, 999)) 0
     forM_ ins (applyInstruction arr)
     return arr
 
-applyInstruction :: AST.STUArray s LOC Bool -> Instruction -> ST s ()
+applyInstruction :: AST.STUArray s LOC Int -> Instruction -> ST s ()
 applyInstruction arr i =
   forM_ (toLOCs . _range $ i) $ \loc -> do
     e <- AST.readArray arr loc
@@ -79,14 +79,13 @@ applyInstruction arr i =
   where
     f = toF i
 
-ins s = fromRight [] $ parse instructions "" s
-
-first :: String -> IO ()
-first s = do
-  print . length . filter id . AU.elems . applyInstructions $ ins s
+run :: String -> IO ()
+run s = do
+  let ins = fromRight [] $ parse instructions "" s
+  print . sum . AU.elems . applyInstructions $ ins
 
 main :: IO ()
 main = do
   content <- readFile "input.txt"
-  putStr "First: "
-  first content
+  putStr "run: "
+  run content
