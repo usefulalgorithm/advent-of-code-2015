182e79350fcf12c953286a8f16fa959592071cf1
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2020-05-29 15:14:47 +0800

    day25
    Signed-off-by: Tsung-Ju Lii <usefulalgorithm@gmail.com>

--- src/Days/Day25.hs
+++ src/Days/Day25.hs
@@ -1,9 +1,30 @@
 module Days.Day25 (day25) where
 
+import           Data.Char
+import           Data.List
 import           Days.ReadPuzzle
 
 day25 :: IO ()
 day25 = do
   input <- readPuzzle 25
-  putStr "First: "
-  print input
+  putStr "Answer: "
+  let [row, col] = parseCoordinate input
+  print $ foldl' (\r _ -> next r) start [1..which (row, col) - 1]
+
+start :: Integer
+start = 20151125
+
+multiplier :: Integer
+multiplier = 252533
+
+modulo :: Integer
+modulo = 33554393
+
+next :: Integer -> Integer
+next n = (n * multiplier) `mod` modulo
+
+which :: (Int, Int) -> Int
+which (row, col) = sum [1..col] + sum (take (row - 1) [col..])
+
+parseCoordinate :: String -> [Int]
+parseCoordinate = map (read . init) . filter (isNumber . head) . words
